5258192fae1c58517a1324778a1a05281400dc54
whonore <wolfhonore@gmail.com>
2023-01-03 11:24:09 -0500

    build: use flake-utils

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Build my website";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.gitignore = {
     url = "github:hercules-ci/gitignore.nix";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -10,29 +11,22 @@
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     gitignore,
-  }: let
-    inherit (gitignore.lib) gitignoreSource;
-    build-pkgs = {
-      system,
-      dev ? false,
-    }: let
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (gitignore.lib) gitignoreSource;
       pkgs = nixpkgs.legacyPackages.${system};
-      py-packages = python-packages:
-        with python-packages;
-          [pyshp]
-          ++ (pkgs.lib.optional dev [black flake8 isort mypy pathspec]);
-      python = pkgs.python3.withPackages py-packages;
-    in [python pkgs.imagemagick pkgs.jpegoptim pkgs.minify pkgs.postcss-cli pkgs.svgcleaner];
-    env = system:
-      nixpkgs.legacyPackages.${system}.mkShell {
-        packages = build-pkgs {
-          inherit system;
-          dev = true;
-        };
-      };
-    website = system:
-      nixpkgs.legacyPackages.${system}.stdenv.mkDerivation {
+      build-pkgs = {dev ? false}: let
+        py-packages = python-packages:
+          with python-packages;
+            [pyshp]
+            ++ (pkgs.lib.optional dev [black flake8 isort mypy pathspec]);
+        python = pkgs.python3.withPackages py-packages;
+      in [python pkgs.imagemagick pkgs.jpegoptim pkgs.minify pkgs.postcss-cli pkgs.svgcleaner];
+
+      env = pkgs.mkShell {packages = build-pkgs {dev = true;};};
+      website = pkgs.stdenv.mkDerivation {
         pname = "website";
         version = "0.0.0";
         src = gitignoreSource ./.;
@@ -42,21 +36,12 @@
         postPatch = "patchShebangs scripts";
 
         enableParallelBuilding = true;
-        buildInputs = build-pkgs {inherit system;};
+        buildInputs = build-pkgs {};
 
         installFlags = ["INSTALL_DIR=${placeholder "out"}"];
       };
-  in {
-    packages.aarch64-darwin.default = website "aarch64-darwin";
-    packages.aarch64-linux.default = website "aarch64-linux";
-    packages.i686-linux.default = website "i686-linux";
-    packages.x86_64-darwin.default = website "x86_64-darwin";
-    packages.x86_64-linux.default = website "x86_64-linux";
-
-    devShells.aarch64-darwin.default = env "aarch64-darwin";
-    devShells.aarch64-linux.default = env "aarch64-linux";
-    devShells.i686-linux.default = env "i686-linux";
-    devShells.x86_64-darwin.default = env "x86_64-darwin";
-    devShells.x86_64-linux.default = env "x86_64-linux";
-  };
+    in {
+      packages.default = website;
+      devShells.default = env;
+    });
 }
